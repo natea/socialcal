1603d1e0720238c3a1dc1d34d420d375e88f9574
Nate Aune <natejaune@gmail.com>
2025-02-04 15:46:19 -0500

    fix all the views where google login is required to show the google icon

--- core/views.py
+++ core/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.urls import reverse
 
 def home(request):
-    return render(request, 'core/home.html')
+    if request.user.is_authenticated:
+        # For authenticated users, redirect to the week view with current date
+        today = timezone.now()
+        return redirect(reverse('calendar:week', kwargs={
+            'year': today.year,
+            'month': today.month,
+            'day': today.day
+        }))
+    else:
+        # For unauthenticated users, redirect to the onboarding welcome page
+        return redirect('onboarding:welcome')
 
 def about(request):
     return render(request, 'core/about.html')
