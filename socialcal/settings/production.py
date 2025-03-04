b22dfb123f952b06d2a233ae5caa4b2d89fe4f63
Nate Aune <natejaune@gmail.com>
2025-03-04 17:09:05 -0500

    Trigger production build with minor configuration update

--- socialcal/settings/production.py
+++ socialcal/settings/production.py
@@ -89,6 +89,8 @@ GROQ_API_KEY = os.environ.get('GROQ_API_KEY', '')
 OLOSTEP_API_KEY = os.environ.get('OLOSTEP_API_KEY', '')
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', '')
 
+# make a small change to trigger a build
+
 # Spotify API Configuration
 SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
